8082852433f2797b69a22b5bc6c11efb2c26a2d6
Mostind <joaopedroalmeida007@gmail.com>
2023-05-01 07:34:40 -0300

    atualizacao1

---
+++ tabelaProd.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset='utf-8'>
+    <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+    <title>Tabela</title>
+    <meta name='viewport' content='width=device-width, initial-scale=1'>
+    <link rel="stylesheet" href="tabelaG.css">
+
+</head>
+<body>
+  <main>
+      
+        <!-- Os botões que ficam dentro de título -->
+        <div class="tituloBot">
+            <a href="" class="botTit">Voltar</a>
+            <h3>PRODUTOS</h3>
+            <a href="" class="botTit">Novo Categoria</a>
+        </div>
+      
+         <!--A primeira parte da tabela-->
+      <div class="grupo">
+            <div class="sub-grupo">IDUSER</div>
+            <div class="sub-grupo">NOME</div>
+ 
+      </div>
+           <!-- A segunda parte da tabela  -->
+           <div class="grupo2">
+               <div class="sub-grupo2">1</div>
+               <div class="sub-grupo2">JOÃO PEDRO DE ALMEIDA</div>
+
+               <!-- Botões dentro da tabela -->
+               <div class="sub-grupo2"><input type="submit" value="Enviar"> </div>
+               <div class="sub-grupo2"><input type="submit" value="Deletar"></div>
+           </div>
+           
+           <div class="grupo2">
+            <div class="sub-grupo2">2</div>
+            <div class="sub-grupo2">Alberto Ferraira de Sousa</div>
+
+            <!-- Botões dentro da tabela -->
+            <div class="sub-grupo2"><input type="submit" value="Enviar"> </div>
+            <div class="sub-grupo2"><input type="submit" value="Deletar"></div>
+        </div>
+        
+        <div class="grupo2">
+            <div class="sub-grupo2">3</div>
+            <div class="sub-grupo2">JOÃO PEDRO DE ALMEIDA</div>
+
+            <!-- Botões dentro da tabela -->
+            <div class="sub-grupo2"><input type="submit" value="Enviar"> </div>
+            <div class="sub-grupo2"><input type="submit" value="Deletar"></div>
+        </div>
+        
+        <div class="grupo2">
+            <div class="sub-grupo2">4</div>
+            <div class="sub-grupo2">JOÃO PEDRO DE ALMEIDA</div>
+
+            <!-- Botões dentro da tabela -->
+            <div class="sub-grupo2"><input type="submit" value="Enviar"> </div>
+            <div class="sub-grupo2"><input type="submit" value="Deletar"></div>
+        </div>
+        
+        </div>
+  </main>
+
+</body>
+</html>
